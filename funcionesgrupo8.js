e028c81ea1cbe53a6917341ceadd34ad49a7fbdb
cristianseb07 <91921855+cristianseb07@users.noreply.github.com>
2021-11-24 23:15:52 -0300

    Add files via upload

---
+++ funcionesgrupo8.js
@@ -0,0 +1,30 @@
+Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Su Consulta se ha enviado correctamente!',
+    showConfirmButton: false,
+    timer: 1500
+  })
+
+
+function mostrar(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Su Consulta se ha enviado correctamente!',
+        showConfirmButton: false,
+        timer: 5000
+      });
+}
+
+function mostrar1(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Su ha registrado correctamente!',
+        showConfirmButton: false,
+        timer: 5000
+      });
+}
+
+
